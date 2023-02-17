bc0e35fed635208f110b06bf18ccddfaec48ed85
Max <0717436@schoolsnet.act.edu.au>
2023-02-17 12:53:39 +1100

    Added Countdown, keys, and collison

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -8,5 +8,11 @@ var currentTimer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	currentTimer = countdownMax
-	$HUD/Countdown.text = str(currentTimer)
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over")
 
